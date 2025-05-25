f05cdddd67686e9f01d6a899cd67a0637c8132d9
hatef2003 <hatef.1382@gmail.com>
2025-05-25 17:05:32 +0330

    asdadas

--- WebSocketCommunicator/client.py
+++ WebSocketCommunicator/client.py
@@ -1,18 +1,23 @@
 import asyncio
 import json
 from WebSocketCommunicator import WebSocketWrapper
-
+from PIL import Image
+import io
+import base64
 async def main():
     ws = WebSocketWrapper("192.168.43.8", 8125)
     await ws.connect()
 
     # Send JSON
-    await ws.send(json.dumps({"type": "greeting", "content": "Hello Server!"}))
+    # await ws.send(json.dumps({"type": "greeting", "content": "Hello Server!"}))
     
     response = await ws.receive_json()
 
-    print("Received JSON response:", response['content'])
-
+    image = response["payload"]
+    image_bytes = base64.b64decode(image)
+    # image= image.encode("utf-8")
+    image = Image.open(io.BytesIO(image_bytes))
+    image.show()
     await ws.close()
 
 asyncio.run(main())
--- WebSocketCommunicator/server.py
+++ WebSocketCommunicator/server.py
@@ -2,20 +2,25 @@
 import asyncio
 import websockets
 import json
-
+from PIL import Image
+import io
+import base64
 async def handle_connection(websocket):
-    async for message in websocket:
-        print(f"Received: {message}")
-        data = json.loads(message)
-        
         # Respond with a simple message
-        response = {"type": "response", "content": "Hello, Client!"}
-        await websocket.send(json.dumps(response))
+    with open("C:\\Users\\Hot-f\\Desktop\\photo_2025-05-13_17-43-47.jpg", "rb") as f:
+        image_data = f.read()
+    
+    i = Image.open(io.BytesIO(image_data))
+    # i.show()
+    image_base64 = base64.b64encode(image_data).decode('utf-8')
+    response = {"type": "response", "payload":image_base64}
+    await websocket.send(json.dumps(response))
 
 async def main():
     # Start the server on 0.0.0.0:8125
     async with websockets.serve(handle_connection, "0.0.0.0", 8125):
         print("WebSocket server started on ws://0.0.0.0:8125")
         await asyncio.Future()  # run forever
+
 
 asyncio.run(main())
