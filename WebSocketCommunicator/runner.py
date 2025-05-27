349de5ee4aafa57a31cc342d8561c7b10295f5db
hatef2003 <hatef.1382@gmail.com>
2025-05-27 17:23:16 +0330

    Ad

--- WebSocketCommunicator/runner.py
+++ WebSocketCommunicator/runner.py
@@ -3,8 +3,8 @@ from PIL import Image, ImageDraw, ImageFont
 import tflite_runtime.interpreter as tflite
 import time
 
-model_path = 'y8.tflite'
-interpreter = tflite.Interpreter(model_path=model_path)
+model_path = 'trueY8.tflite'
+interpreter = tflite.Interpreter(model_path=model_path,experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
